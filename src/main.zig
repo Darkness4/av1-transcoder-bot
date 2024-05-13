dcb4cb09b79dc1299a80aa8f8eb3d7b97941e3f6
Nguyen Marc <nguyen_marc@live.fr>
2024-05-13 02:39:02 +0200

    fix(main): assert memory leaks

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,8 @@ const av = @import("av.zig");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() == .ok);
+
     const allocator = gpa.allocator();
 
     var args = std.ArrayList([*:0]const u8).init(allocator);
